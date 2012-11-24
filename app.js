f5db901ce01d51e5873243715e2477caaa1a5136
Dan Palmer <dan.palmer@me.com>
2012-11-24 22:09:48 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var flatiron = require("flatiron");
+var redis = require("redis").createClient();
+var fs = require('fs');
+var images = require('./photos').all;
+var app = flatiron.app;
+
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+  redis.auth(rtg.auth.split(":")[1]);
+} else {
+  var redis = require("redis").createClient();
+}
+
+app.use(flatiron.plugins.http, {
+  // HTTP options
+});
+
+app.router.get('/', function() {
+  var mp1 = Math.floor(Math.random() * images.length);
+  var mp2 = Math.floor(Math.random() * images.length);
+
+  this.res.writeHead(200, { 'Content-Type': 'text/plain' });
+  this.res.end(mp1 + '\n' + mp2);
+});
+
+app.start(process.env.PORT || 3000);
